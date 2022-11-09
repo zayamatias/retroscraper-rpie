e338520c62ee2a661866041fcdaeb42659268403
zayamatias <32246591+zayamatias@users.noreply.github.com>
2022-11-09 14:22:52 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt update -y
+sudo apt upgrade -y
+wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
+python3 /tmp/get-pip.py
+python3 -m pip install --user --upgrade pip wheel setuptools
+python3 -m pip install --user -r "$1/dependencies.txt"
