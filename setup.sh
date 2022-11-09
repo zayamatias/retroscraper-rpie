15ae4afcc501753b169e016b6e2d0a14d598650b
zayamatias <32246591+zayamatias@users.noreply.github.com>
2022-11-09 15:10:56 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo apt update -y
-sudo apt upgrade -y
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 python3 /tmp/get-pip.py
 python3 -m pip install --user --upgrade pip wheel setuptools
