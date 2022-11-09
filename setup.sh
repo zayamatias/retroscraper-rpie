b648cccd9df4a86e4d733c3d6ae5c975cf4e4b99
zayamatias <32246591+zayamatias@users.noreply.github.com>
2022-11-09 14:25:24 +0000

    Update setup.sh
    This file will only run under retropie-setup

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ sudo apt upgrade -y
 wget https://bootstrap.pypa.io/get-pip.py -O /tmp/get-pip.py
 python3 /tmp/get-pip.py
 python3 -m pip install --user --upgrade pip wheel setuptools
-python3 -m pip install --user -r "dependencies.txt"
+python3 -m pip install --user -r "$1/dependencies.txt"
